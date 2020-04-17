add316227bc8588544253f69f08419845176fa97
Purvil Mehta <purvilmehta06@gmail.com>
2020-04-17 20:51:03 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Almost_there_5:SmartBricks
+## Tools
+1) Download nodejs and install that software
+	link : [https://nodejs.org/en/download/](https://nodejs.org/en/download/)
+2) Download Visual Code and install the software
+	link : [https://code.visualstudio.com/](https://code.visualstudio.com/)
+3) Open visual studio and download some extensions from left pannel
+	1) Live Server
+	2) JavaScript (ES6) code snippets
+	3) formatter-pug
+	3) code-runner
+4) Download mongoDB software
+	link : [https://www.mongodb.com/download-center/community](https://www.mongodb.com/download-center/community)
+5) Install mongoDB
+	1) install mongoDB on custom method
+	2) Browse the folder and select C drive itself. Then make new folder name mongodb in C drive and choose that folder to install software.
+	3) On the next page, append '\db' in data file path. ***Remember not to change log file path. Just change data path to 'C:\mongodb\data\db'.***
+	4) On the next page, uncheck ***Install MongoDB Compass***. Then press install. It might ask for restarting your PC. 
+6) Open CMD as administator and set path to ***C:\mongodb\bin*** and run ***`net start mongodb`***. Ignore if mongoDB is already activated.
+
+## Mongodb Controls
+1) Open CMD as administator and set path to ***C:\mongodb\bin***
+2) Type ***`mongo`*** on terminal.
+3) Type ***`>use trial2`*** on command line.
+
+## How to run Code ?? 
+1) Open VS Code and press 
+<kbd>Ctrl +`</kbd>
+2) Open folder **Almost_there_5** which you have downloaded from here.
+3) ***``npm install``***
+4) ***``npm run dev``***
+5) Open Chrome browser and type ***``http://localhost:5000``*** 
+
+
+## Remaining Work
+
+1) Statistic Page - Frontend and backend
+2) Other tools Price estimator etc.., if time permits.
---
+++ models/property.js
@@ -0,0 +1,114 @@
+let mongoose = require('mongoose')
+
+let propertySchema = mongoose.Schema({
+    Name:{
+        type:String,
+        require: true
+    },
+    DATE : {
+        type:Date,
+        require: true
+    },
+    Description:{
+        type:String,
+        required: true
+    },
+    isBuilder:{
+        type:String,
+        required: true
+    },
+    Location:{
+        type:String,
+        required: true
+    },
+    lat:{
+        type: Number,
+        required: true
+    },
+    lon:{
+        type: Number,
+        required: true
+    },
+    Sqfoot:{
+        type: Number,
+        required: true
+    },
+    BHK:{
+        type: Number,
+        required : true
+    },
+    ownerid:{
+        type: String,
+        required: true
+    },
+    zipcode:{
+        type: String,
+        required : true
+    },
+    Gym:{
+        type: Boolean,required: true
+    },
+    Lawn_Free_Space:{
+        type: Boolean,required: true
+    },
+    Security:{
+        type: Boolean,required: true
+    },
+    CCTV:{
+        type: Boolean,required: true
+    },
+    School_within_5:{
+        type: Boolean,required: true
+    },
+    Hospital_within_5:{
+        type: Boolean,required: true
+    },
+    Railway_within_5:{
+        type: Boolean,required: true
+    },
+    Airport_within_5:{
+        type: Boolean,required: true
+    },
+    water:{
+        type: Boolean,required: true
+    },
+    electricity:{
+        type: Boolean,required: true
+    },
+    Lift:{
+        type: Boolean,required: true
+    },
+    Club_House:{
+        type: Boolean,required: true
+    },
+    Price:{
+        type: Number,required:true
+    },
+    Owner:{
+        type:String,required:true
+    },
+    images:{
+        type: [String],required:true
+    },
+    verified:{
+        type: Boolean,required:true
+    },
+    req_verified:{
+        type: Boolean,required:true
+    },
+    cityFlag:{
+        type: Boolean,required:true,default:true
+    },
+    maxFlag:{
+        type: Boolean,required:true,default:true
+    },
+    bhkFlag:{
+        type: Boolean,required:true,default:true
+    },
+    areaFlag:{
+        type: Boolean,required:true,default:true
+    }
+})
+let Property = module.exports = mongoose.model('Property',propertySchema);
+
+//Property.createIndexes({'Location':'text'});
